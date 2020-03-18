0429ab83f8dc1a63cf3e9a2a63ae4af24add1f9e
unknown <jake11b4v@gmail.com>
2020-03-18 17:10:38 -0500

    Created Employee mode class with properties

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,8 @@ namespace TrashCollector.Models
 {
     public class Customer
     {   
-         public int CustomerId { get; set; }
+        [Key]
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PickupDay { get; set; }
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ZipCode { get; set; }
+
+        //[ForeignKey("IdentityUser")]
+
+        //public string IdentityUserId { get; set; }
+        //public IdentityUser IdentityUser { get; set; }   
+
+    }
+}
