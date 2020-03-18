9856f57f24905f3f3f99eebba984ca7c027438e2
unknown <jake11b4v@gmail.com>
2020-03-18 18:53:05 -0500

    Created CustomerController and EmployeeController with scaffolding

--- TrashCollector/Data/ApplicationDbContext.cs
+++ TrashCollector/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrashCollector.Models;
 
 namespace TrashCollector.Data
 {
@@ -33,5 +34,19 @@ namespace TrashCollector.Data
           
 
         }
+
+        //public DbSet<IdentityUser> IdentityUsers { get; set; }
+
+        //public DbSet<Customer> Customers { get; set; }
+
+        //public DbSet<Employee> Employees { get; set; }
+        public DbSet<TrashCollector.Models.Customer> Customer { get; set; }
+
+        //public DbSet<IdentityUser> IdentityUsers { get; set; }
+
+        //public DbSet<Customer> Customers { get; set; }
+
+        //public DbSet<Employee> Employees { get; set; }
+        public DbSet<TrashCollector.Models.Employee> Employee { get; set; }
     }
 }
